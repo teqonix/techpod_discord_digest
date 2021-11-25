a07a9bede9dc921e50b25a60ef3416eef28faf65
teqonix <teqonix@gmail.com>
2021-11-24 23:34:52 -0700

    Fix custom docker build command for amd64

--- src/docker/docker_buildx_build.sh
+++ src/docker/docker_buildx_build.sh
@@ -1,5 +1,6 @@
 docker buildx build \
   --platform linux/amd64 \
   -t $IMAGE \
+  --build-arg application_credentials=/usr/local/digest_bot/secret_store/techpod-discord-digest-0d66139b52b6.json \
   --push \
   $BUILD_CONTEXT
